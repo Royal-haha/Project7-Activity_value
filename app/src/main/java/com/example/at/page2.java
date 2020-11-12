ec56e264373b84218640157b01d88ed7d62605cf
RAY <lee8765012@gmail.com>
2020-11-12 15:46:26 +0800

    Project7-Activity_value
    project中有兩個activity, 各有一個button、可以輸入的editView, 點擊A activity中的button會跳到B activity, 並將輸入的文字傳送到，Ｂactivity。點擊B activity中的button會跳到A activity，並將Ｂactivity的數值顯示在A頁面中。

---
+++ app/src/main/java/com/example/at/page2.java
@@ -0,0 +1,68 @@
+package com.example.at;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class page2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        TextView k = (TextView)findViewById(R.id.textView5);
+        Intent intent = getIntent();
+        String user_name = intent.getStringExtra("USER_NAME");
+        k.setText(user_name);
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                Intent intent = new Intent(page2.this, page1.class);
+                EditText x = (EditText) findViewById(R.id.editTextTextPersonName3);
+                String user_name = x.getText().toString();
+                intent.putExtra("USER_NAME", user_name);
+                startActivity(intent);
+
+            }
+        });
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+}
